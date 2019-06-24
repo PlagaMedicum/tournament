278b603221d4dff0bd8d70160753daad6a1d0dbf
Alexander Klyuev <katoshrodingera@protonmail.ch>
2019-06-24 07:50:17 +0300

    some db stuff

--- src/main.go
+++ src/main.go
@@ -1,33 +1,66 @@
 package main
 
 import (
+	"encoding/json"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type User struct{
-	id uint
-	name string
+	id 		string
+	name 	string
 	balance int
 }
 
 type Tournament struct {
-	id uint
-	name string
+	id 		string
+	name  	string
 	deposit int
-	prize int
-	users []int
-	winner int
+	prize   int
+	users   []int
+	winner  int
 }
+
+var (
+	userList   []User
+	tournament []Tournament
+)
 
 func createUser(response http.ResponseWriter, request *http.Request){
 	if request.Method == "POST"{
+		response.Header().Set("Content-Type", "application/json")
+		var user User
+		err := json.NewDecoder(request.Body).Decode(&user); if err != nil {
+			panic(err)
+		}
+		user.id = strconv.Itoa(rand.Intn(1000000))
+		user.balance = 700
+		userList = append(userList, user)
+
 		response.WriteHeader(201)
+		err = json.NewEncoder(response).Encode(user.name); if err != nil {
+			panic(err)
+		}
 	}
 }
 
 func processUser(response http.ResponseWriter, request *http.Request){
 	switch request.Method {
 	case "GET":
+		response.Header().Set("Content-Type", "application/json")
+		id := request.URL.Path[len("/user/"):]
+		for _, item := range userList {
+			if item.id == id {
+				err := json.NewEncoder(response).Encode(item.name); if err != nil {
+					panic(err)
+				}
+				return
+			}
+		}
+		err := json.NewEncoder(response).Encode(&User{}); if err != nil {
+			panic(err)
+		}
 		response.WriteHeader(200)
 	case "DELETE":
 		response.WriteHeader(200)
@@ -75,7 +108,7 @@ func finishTournament(response http.ResponseWriter, request *http.Request){
 
 func main(){
 	http.HandleFunc("/user", createUser)
-	http.HandleFunc("/user/{id}", processUser)
+	http.HandleFunc("/user/", processUser)
 	http.HandleFunc("user/{id}/take", subtractUserPoints)
 	http.HandleFunc("user/{id}/fund", addUserPoints)
 	http.HandleFunc("/tournament", createTournament)
@@ -83,7 +116,8 @@ func main(){
 	http.HandleFunc("/tournament/{id}/join", joinTournament)
 	http.HandleFunc("/tournament/{id}/finish", finishTournament)
 
-	if err := http.ListenAndServe(":9090", nil); err != nil{
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
 		panic(err)
 	}
 }
