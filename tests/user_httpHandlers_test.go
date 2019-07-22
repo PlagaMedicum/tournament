818de7ad0e72919c0180e0473f83cb6660f9776b
Alexander Klyuev <katoshrodingera@protonmail.ch>
2019-07-22 10:19:36 +0300

    started TestGetUserHandler

--- tests/user_httpHandlers_test.go
+++ tests/user_httpHandlers_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 	"tournament/env/myhandler"
 	"tournament/pkg/router"
-	tournament "tournament/pkg/tournament/model"
 	user "tournament/pkg/user/model"
 )
 
@@ -18,13 +17,17 @@ type tester struct {
 	path 				string
 	method				string
 	methodName 			string
+	resultErr			error
+	resultID			uuid.UUID
 	requestUser			user.User
-	requestTournament 	tournament.Tournament
 	requestBody			[]byte
 	expectedStatus 		int
 }
 
-func handleTester(t *testing.T, h myhandler.Handler, tr tester){
+func handleTester(t *testing.T, mo *mockedUser, tr tester) {
+	h := myhandler.Handler{}
+	router.RouteForUser(&h, mo)
+
 	r, err := http.NewRequest(
 		tr.method, tr.path, bytes.NewBuffer(tr.requestBody))
 	if err != nil {
@@ -49,6 +52,8 @@ func TestCreateUserHandling(t *testing.T) {
 			path:       router.UserPath,
 			method:     http.MethodPost,
 			methodName: "CreateUser",
+			resultErr:	nil,
+			resultID:	uuid.NewV1(),
 			requestUser: user.User{
 				Name: "Daniil Dankovskij",
 			},
@@ -68,6 +73,8 @@ func TestCreateUserHandling(t *testing.T) {
 			path:       router.UserPath,
 			method:     http.MethodPost,
 			methodName: "CreateUser",
+			resultErr:	errors.New("i'm the bad err"),
+			resultID:	uuid.NewV1(),
 			requestUser: user.User{
 				Name: "Artemij Burah",
 			},
@@ -77,14 +84,44 @@ func TestCreateUserHandling(t *testing.T) {
 	}
 
 	mo := mockedUser{}
-	mo.On(trList[0].methodName, trList[0].requestUser).Return(uuid.NewV1(), nil)
-	mo.On(trList[2].methodName, trList[2].requestUser).Return(uuid.NewV1(), errors.New("wrong user error"))
-
-	h := myhandler.Handler{}
-	router.RouteForUser(&h, &mo)
 
 	for _, tr := range trList {
-		handleTester(t, h, tr)
+		mo.On(tr.methodName, tr.requestUser).Return(tr.resultId, tr.resultErr)
+		handleTester(t, &mo, tr)
+	}
+
+	mo.AssertExpectations(t)
+}
+
+// TestCreateUser tests getting of user information.
+func TestGetUserHandling(t *testing.T) {
+	sampleID := uuid.NewV1()
+	trList := []tester{
+		{
+			caseName:   "everything ok",
+			path:       router.UserPath+sampleID,
+			method:     http.MethodPost,
+			methodName: "GetUser",
+			resultErr:	nil,
+			resultUser: ,
+			expectedStatus: http.StatusCreated,
+		},
+		{
+			caseName:   "wrong user error",
+			path:       router.UserPath,
+			method:     http.MethodPost,
+			methodName: "GetUser",
+			resultErr:	errors.New("i'm the bad err"),
+			resultUser: ,
+			expectedStatus: http.StatusBadRequest,
+		},
+	}
+
+	mo := mockedUser{}
+
+	for _, tr := range trList {
+		mo.On(tr.methodName, tr.requestUser).Return(tr., tr.resultErr)		
+		handleTester(t, &mo, tr)
 	}
 
 	mo.AssertExpectations(t)
